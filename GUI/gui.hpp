ba256bd90b44fc9d3663398fbe0ed8694a7e98fd
Your Name <you@example.com>
2025-11-07 19:31:23 +0200

    gui

--- GUI/gui.hpp
+++ GUI/gui.hpp
@@ -1,11 +1,16 @@
 #ifndef GUI_HPP
 #define GUI_HPP
 
+#include <SDL2/SDL.h>
+#include "../Core/Chess.hpp"
+
 namespace GUI {
+	constexpr int TILE_SIZE = 80;
+	constexpr int WIDTH = TILE_SIZE * Chess::BOARD_SIZE;
+	constexpr int HEIGHT = TILE_SIZE * Chess::BOARD_SIZE;
 
-}
+	void drawChessBoard(SDL_Renderer* renderer);
 
-
-
+};
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,35 +1,35 @@
-//#include <iostream>
-//#include "Core\MoveGen.hpp"
-//#include <iomanip>
-//#include "Core\BoardState.hpp"
-//#include "Test\Test.hpp"
-//#include "Utils\Zobrist.hpp"
-//#include "Utils\Fen.hpp"
-//#include <array>
-//
-//bool init(Game& game) {
-//	Zobrist::init();
-//	MoveGen::init();
-//	bool color = Fen::handleFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", game);
-//	if (color) game.init<true, true>();
-//	else game.init<false, true>();
-//
-//	return color;
-//}
-//int main() {
-//	Game game;
-//	bool color = init(game);
-//
-//	if (color) {
-//		//Test::showAfterMoveFens<true>(game);
-//		//Test::perftPerMove<true>(game, 1);
-//		//Test::makeMove<true>(game, Square::getIndex("g7"), Square::getIndex("a1"));
-//		Test::loopedTimedPerft<true>(game, 1, 7);
-//	}
-//	else {
-//		//Test::showAfterMoveFens<false>(game);
-//		//Test::perftPerMove<false>(game, 1);
-//		//Test::makeMove<false>(game, Square::getIndex("g7"), Square::getIndex("a1"));
-//		Test::loopedTimedPerft<false>(game, 1, 7);
-//	}
-//}
+#include <iostream>
+#include "Core\MoveGen.hpp"
+#include <iomanip>
+#include "Core\BoardState.hpp"
+#include "Test\Test.hpp"
+#include "Utils\Zobrist.hpp"
+#include "Utils\Fen.hpp"
+#include <array>
+
+bool init(Game& game) {
+	Zobrist::init();
+	MoveGen::init();
+	bool color = Fen::handleFen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", game);
+	if (color) game.init<true, true>();
+	else game.init<false, true>();
+
+	return color;
+}
+int main() {
+	Game game;
+	bool color = init(game);
+
+	if (color) {
+		//Test::showAfterMoveFens<true>(game);
+		//Test::perftPerMove<true>(game, 1);
+		//Test::makeMove<true>(game, Square::getIndex("g7"), Square::getIndex("a1"));
+		Test::loopedTimedPerft<true>(game, 1, 7);
+	}
+	else {
+		//Test::showAfterMoveFens<false>(game);
+		//Test::perftPerMove<false>(game, 1);
+		//Test::makeMove<false>(game, Square::getIndex("g7"), Square::getIndex("a1"));
+		Test::loopedTimedPerft<false>(game, 1, 7);
+	}
+}
